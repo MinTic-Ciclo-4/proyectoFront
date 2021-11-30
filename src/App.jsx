33c3958e6af63766b2b1ff747578ac579827609b
Johan Forero <johanfore67@gmail.com>
2021-11-30 11:22:26 -0500

    Sidebar - Rutas - y Responsive Menu - COnfiguracion Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import PrivateLayout from './layouts/PrivateLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Auth0Provider } from '@auth0/auth0-react';
+// import { UserContext } from 'context/userContext';
+// import Index from 'pages/Index';
+// import Page2 from 'pages/Page2';
+// import IndexCategory1 from 'pages/category1/Index';
+// import Category1 from 'pages/category1/CategoryPage1';
+import './styles/globals.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PrivateLayout />}>
+          {/* <Route path='' element={<Index />} /> */}
+          {/* <Route path='page2' element={<Page2 />} /> */}
+          {/* <Route path='category1' element={<IndexCategory1 />} /> */}
+          {/* <Route path='category1/page1' element={<Category1 />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
